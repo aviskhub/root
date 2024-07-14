187c4b65049b20414c21286e25f249a4e4bf09b3
Singh <asingh30@deloitte.com>
2024-07-14 10:19:08 +0530

    testing

--- api/sayingthanks/lambda_function.py
+++ api/sayingthanks/lambda_function.py
@@ -1,4 +1,8 @@
-import boto3
+import json
 
-
-
+def lambda_handler(event, context):
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
