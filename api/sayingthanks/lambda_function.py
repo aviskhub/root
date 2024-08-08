e873d203ca6868171dd4c9eee0b9f1c86a51c1cd
Singh <asingh30@deloitte.com>
2024-08-08 17:04:04 +0530

    changing it

--- api/sayingthanks/lambda_function.py
+++ api/sayingthanks/lambda_function.py
@@ -1,11 +1,11 @@
 import json
 import boto3
-
+import requests
 client = boto3.client('ses')
 def lambda_handler(event, context):
     # TODO implement
     print("printing all the environment variable", os.environ)
-   
+    r = requests.get('https://api.github.com')
     return { 
         'statusCode': 200,
         'body': json.dumps()
--- terraform/main.tf
+++ terraform/main.tf
@@ -78,7 +78,7 @@ resource "aws_lambda_layer_version" "lambda-layer" {
   s3_key = "api/layer/lambda_layer.zip"
   s3_object_version = data.aws_s3_object.lambda-layer.version_id
 } 
- 
+  
  
 
 
